e0a4ecc985da455ff7a684b442062b811402e468
Jesse Gallagher <jesse@secondfoundation.org>
2023-06-26 10:06:31 -0400

    Fix up JAX-B package-info files a bit

--- nsf-home/odp/Code/Java/api/rss20/model/package-info.java
+++ nsf-home/odp/Code/Java/api/rss20/model/package-info.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// TODO figure out why JAX-B doesn't pick up on these
 @XmlSchema(
 	xmlns = {
 		@XmlNs(namespaceURI=Rss.NS_CONTENT, prefix="content"),
