6a837d79160e870d689af03b21cd98fb671255bf
Jesse Gallagher <jesse@secondfoundation.org>
2026-05-01 12:43:52 -0400

    Remove unused import

--- nsf-home/odp/Code/Java/controller/projects/ScreenshotsController.java
+++ nsf-home/odp/Code/Java/controller/projects/ScreenshotsController.java
@@ -20,7 +20,6 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.NotAuthorizedException;
-import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
